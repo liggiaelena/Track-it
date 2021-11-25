868f5038409fa565a12d2461c786e81bdc152deb
liggia <liggia.cruz@gmail.com>
2021-11-25 15:00:13 -0300

    feat: App.js

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import LoginPage from './components/LoginPage/LoginPage';
+import SingUpPage from './components/SingUpPage/SingUpPage';
+import HabitsPage from './components/HabitsPage/HabitsPage';
+import TodayPage from './components/TodayPage/TodayPage';
+import HistoricPage from './components/HistoricPage/HistoricPage';
+import './index.css';
+import UserContext from './contexts/UserContext';
+import { useState } from 'react';
+
+function App(){
+    const [token, setToken]= useState("");
+    const [percentage, setPercentage] = useState(0);
+    const [photo, setPhoto] = useState("")
+  
+
+    return(
+        <UserContext.Provider value={{token, setToken, percentage, setPercentage, setPhoto, photo}}>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+                <LoginPage /> 
+            </Route>
+             <Route path="/cadastro" exact>
+                <SingUpPage /> 
+            </Route>
+            <Route path="/habitos" exact>
+                <HabitsPage /> 
+            </Route>
+            <Route path="/hoje" exact>
+                <TodayPage />
+            </Route>
+            <Route path="/historico" exact>
+                <HistoricPage />
+            </Route>    
+        </Switch>     
+    </BrowserRouter>
+        </UserContext.Provider>
+    
+
+    );
+}
+
+export default App
